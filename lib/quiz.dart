2567d93ade17e2b57051ad63adfbc7879864ffa1
Alisher <tukushevalisher123@gmail.com>
2025-02-01 13:12:44 +0500

    first commit

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,31 +1,69 @@
+// ignore_for_file: non_constant_identifier_names
+
 import 'package:flutter/material.dart';
-import 'package:adv_basics2/start_screen.dart';
+import 'package:quiz_app/data/questions.dart';
+import 'package:quiz_app/question_screen.dart';
+import 'package:quiz_app/start_screen.dart';
+import 'package:quiz_app/results_screen.dart';
 
 class Quiz extends StatefulWidget {
-  const Quiz({super.key});
+  const Quiz({super.key}); //accept and forward name key argument
 
   @override
-  State<Quiz> createState() => _QuizState();
+  State<Quiz> createState() {
+    return _QuizState();
+  }
 }
 
 class _QuizState extends State<Quiz> {
+  //'_' this will make this class private and can only be used in this file
+  final List<String> selectedAnswers = [];
+  var activescreen = 'start-screen';
+
+  void SwitchScreen() {
+    setState(() {
+      activescreen = 'question-screen';
+    });
+  }
+
+  void chooseAnswer(String answer) {
+    selectedAnswers.add(
+        answer); //we are not manipulating the string here, we are just adding up the new answers so the string can be final
+    if (selectedAnswers.length == questions.length) {
+      setState(() {
+        activescreen = 'results-screen';
+      });
+    }
+  }
+
   @override
-  Widget build(BuildContext context) {
+  Widget build(context) {
+    Widget screenwidget = StartScreen(SwitchScreen);
+
+    if (activescreen == 'question-screen') {
+      screenwidget = question_Screen(
+        onSelectAnswer: chooseAnswer,
+      );
+    }
+
+    if (activescreen == 'results-screen') {
+      screenwidget = ResultsScreen(
+        chosenAnswers: selectedAnswers,
+      );
+    }
+
     return MaterialApp(
       home: Scaffold(
         body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 78, 13, 151),
-                Color.fromARGB(255, 107, 15, 168),
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
+            decoration: const BoxDecoration(
+              gradient: RadialGradient(
+                colors: [
+                  Colors.deepPurpleAccent,
+                  Color.fromARGB(255, 137, 96, 252),
+                ],
+              ),
             ),
-          ),
-          child: const StartScreen(),
-        ),
+            child: screenwidget),
       ),
     );
   }
--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -1,39 +1,47 @@
 import 'package:flutter/material.dart';
 
 class StartScreen extends StatelessWidget {
-  const StartScreen({super.key});
-  
+  const StartScreen(this.startQuiz, {super.key});
+
+  final void Function() startQuiz;
+
   @override
   Widget build(context) {
     return Center(
       child: Column(
-        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Image.asset(
             'assets/images/quiz-logo.png',
-            width:300,
-            color: const Color.fromARGB(150, 255, 255, 255),
+            width: 300,
+            height: 200,
+            color: const Color.fromARGB(150, 227, 223, 223),
           ),
-          const SizedBox(height: 80),
-          const Text(
-            'Learn Flutter the fun way',
-            style: TextStyle(
-              color: Color.fromARGB(255, 237, 223, 252),
-              fontSize: 24,
+          const SizedBox(height: 40),
+          const Center(
+            child: Text(
+              'Learn Flutter The Fun Way!',
+              style: TextStyle(
+                  color: Color.fromARGB(213, 241, 239, 239),
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold),
             ),
-        
           ),
-          const SizedBox(height: 30),
+          const SizedBox(height: 35),
           OutlinedButton.icon(
-            onPressed: () {},
+            onPressed: () {
+              startQuiz(); //when clicked the screen is switched
+            },
             style: OutlinedButton.styleFrom(
-              foregroundColor: Colors.white,
+              foregroundColor: Colors.white38,
             ),
-            icon: Icon(Icons.arrow_right_alt),
-            label: Text('Start Quiz'),
+            icon: const Icon(Icons.arrow_right_alt),
+            label: const Text(
+              'start Quiz',
+            ),
           )
         ],
       ),
-      );
+    );
   }
 }
